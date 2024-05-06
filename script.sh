577733b54b8f9c6da4eebe02d92c34602aa4e73f
wollfoo <nhac.tien1988@gmail.com>
2024-05-07 02:15:30 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Tải Nanominer v3.9.1
 echo "Downloading Nanominer v3.9.1..."
-wget -qO- https://github.com/nanopool/nanominer/releases/download/v3.9.1/nanominer-linux-3.9.1.tar.gz | tar xvz
+wget https://github.com/nanopool/nanominer/releases/download/v3.9.1/nanominer-linux-3.9.1.tar.gz | tar xvz
 cd nanominer-linux-3.9.1
 
 # Tạo tệp tin cấu hình
